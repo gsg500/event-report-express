695e955cdc48057e97a84866e6eacab5c7889f70
jfilinho <47835154+jfilinho@users.noreply.github.com>
2021-11-22 22:32:56 -0300

    comitando navbar e rodape

---
+++ src/CJ/HomeEventExpress.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Rodape from "./RodapeEventExpress"
+
+
+function Home(){
+    
+
+
+
+    return(
+
+       <div>
+           <Rodape/>
+       </div> 
+        
+    )
+
+}
+
+export default Home 
