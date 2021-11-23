987a11857eb1c7a4e19b3bafd0968d5da4311923
jfilinho <47835154+jfilinho@users.noreply.github.com>
2021-11-23 19:36:07 -0300

    estilos e componentes slider

--- src/CJ/FooterStyle.js
+++ src/CJ/FooterStyle.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const Box = styled.div`
-padding: 80px 60px;
+padding: 60px 60px;
 background: black;
 position: absolute;
 bottom: 0;
 width: 100%;
-
 
 @media (max-width: 1000px) {
 	padding: 70px 30px;
@@ -20,7 +19,7 @@ export const Container = styled.div`
     align-items: center;
 	max-width: 1000px;
 	margin: 0 auto;
-	/* background: red; */
+	
 `
 
 export const Column = styled.div`
@@ -33,24 +32,22 @@ margin-left: 60px;
 export const Row = styled.div`
 display: grid;
 grid-template-columns: repeat(auto-fill,
-						minmax(185px, 1fr));
+						minmax(100px, 1fr));
 grid-gap: 20px;
 
-@media (max-width: 1000px) {
-	grid-template-columns: repeat(auto-fill,
-						minmax(200px, 1fr));
-}
+// @media (max-width: 1000px) {
+// 	grid-template-columns: repeat(auto-fill,
+// 						minmax(200px, 1fr));
+// }
 `;
 
 export const FooterLink = styled.a`
 color: #fff;
-margin-bottom: 20px;
+margin-bottom: 0px;
 font-size: 18px;
 text-decoration: none;
 
-&:hover {
-	color: green;
-	transition: 200ms ease-in;
+
 }
 `;
 
