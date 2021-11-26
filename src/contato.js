31f646d1bc463439196f7080f044a635e9a8b594
Guilherme Santos Gomes <guilherme0500@hotmail.com>
2021-11-26 02:06:43 -0300

    correções de rota...

--- src/contato.js
+++ src/contato.js
@@ -1,8 +1,10 @@
 import React from "react";
+import FormComments from "./CG/FormComments";
+
 function contato(){
    return(
        <div>
-       -btc
+       <FormComments />       
        </div>
    )
 }
